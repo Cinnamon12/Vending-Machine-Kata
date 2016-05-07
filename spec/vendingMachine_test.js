994ae250c7c9014c329c17031940febd35167709
Sona <sona.mansharamani42@gmail.com>
2016-05-07 01:37:24 -0400

    First test

--- spec/vendingMachine_test.js
+++ spec/vendingMachine_test.js
@@ -1,10 +1,41 @@
 describe('vending machine', function() {
-  let vm;
+  let vm, validCoins;
   beforeEach(function () {
     vm = new vendingMachineController();
+    validCoins = {
+      "nickel": {
+        "weight": 5.00,
+        "diameter": 21.21,
+        "thickness": 1.95,
+        "value": 0.05
+      },
+      "dime": {
+        "weight": 2.27,
+        "diameter": 17.91,
+        "thickness": 1.35,
+        "value": 0.10
+      },
+      "quarter": {
+        "weight": 5.67,
+        "diameter": 24.26,
+        "thickness": 1.75,
+        "value": 0.25
+      }
+    };
   });
 
   it('is set up correctly', function() {
     expect(vm).toBeTruthy();
   });
+
+  it('accepts nickels', function() {
+    let coinAdded = {
+      "weight": 5.00,
+      "diameter": 21.21,
+      "thickness": 1.95
+    };
+    expect(vm.isCoinValid(coinAdded, validCoins)).toBeTruthy();
+  });
+
 });
+
--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -1,3 +1,21 @@
 "use strict";
 function vendingMachineController() {
+  var currentAmount = 0;
+  this.isCoinValid = isCoinValid;
+
+  function isCoinValid (coinAdded, validCoins) {
+    let validProperty;
+    for (var coinType in validCoins){
+      if (validCoins.hasOwnProperty(coinType)){
+        validProperty = validCoins[coinType];
+        if (validProperty.weight === coinAdded.weight && validProperty.diameter === coinAdded.diameter && validProperty.thickness ===
+         coinAdded.thickness) {
+          coinAdded.value = validProperty.value;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
 }
